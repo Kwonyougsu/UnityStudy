6f31ec52d4c2a6424a9cfe59d6e06782f51bd4a9
Kwonyougsu <ysun5311@gmail.com>
2024-05-13 15:02:01 +0900

    필수요구사항 4번까지 구현

---
+++ UpDown_personal assignment/Assets/Script/TopDownAimRotaion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public partial class TopDownAimRotation : MonoBehaviour
+{
+    Transform tf;
+
+    void Start()
+    {
+        tf = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+
+        Vector3 target = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if (target.x < tf.position.x)
+        {
+            tf.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            tf.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
---
+++ UpDown_personal assignment/Assets/Script/TopDownMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public partial class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D movement;
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake() // 주로 컴포넌트안에 종료가 된다
+    {
+        //컨트롤러 == 탑타운 컨트롤러
+        controller = GetComponent<TopDownController>();
+        movement = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movement.velocity = direction;
+
+    }
+}
